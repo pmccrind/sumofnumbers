1d5092ac0b29bc0c63b65c6a9ec9e2a2c1d1f1b7
Patrick McCrindle <35245519+pmccrind@users.noreply.github.com>
2020-02-09 13:43:58 -1000

    Sum of Numbers working as intended.

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,33 +1,37 @@
+const nums = [1, 2, 3];
 function sumFor(someData) {
   let sum = 0;
-  for (let i = 0; i < someData; i++) {
-    sum += someData;
+  for (const num of someData) {
+    sum += num;
   }
   return sum;
 }
+console.log(sumFor(nums)); // 6
 
 function sumWhile(someData) {
   let sum = 0;
   let i = 0;
-  while (i < someData) {
-    sum += someData;
+  while (i <= someData.length) {
+    sum += i;
     i++;
   }
   return sum;
 }
 
+console.log(sumWhile(nums));
+
 function sumRecursion(someData) {
-  if (someData === 0) {
+  if (someData.length === 0) {
     return 0;
   }
-  return (sumRecursion(someData - 1) + 1);
+  const total = someData[0];
+  // slice was got from the webcast, I had no idea that how to complete this task it JS.
+  return sumRecursion(someData.slice(1, someData.length)) + total;
 }
+
+console.log(sumRecursion(nums));
 
 function sumTheSimpleWay(someData) {
-  return _.reduce(someData);
+  return _.reduce(someData, function (memo, num) { return memo + num; });
 }
-
-
-export default {
-  sumFor, sumWhile, sumRecursion, sumTheSimpleWay,
-};
+console.log(sumTheSimpleWay(nums));
