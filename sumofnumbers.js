ace269bfc11339186d8014dd3cd30ed58fcd01a8
Patrick McCrindle <35245519+pmccrind@users.noreply.github.com>
2020-02-09 13:02:36 -1000

    ESLint Corrections complete.
    Needed to re-configure eslint with npm -g i eslint-cli command. Afterwards cleared a single error and the other errors were flagged.

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,35 +1,33 @@
-function sumFor(someData){
+function sumFor(someData) {
   let sum = 0;
-  for (let i = 0; i < someData; i++){
+  for (let i = 0; i < someData; i++) {
     sum += someData;
-
   }
   return sum;
 }
 
 function sumWhile(someData) {
-  let sum= 0;
+  let sum = 0;
   let i = 0;
-  while(i < someData){
+  while (i < someData) {
     sum += someData;
+    i++;
   }
   return sum;
 }
 
-function sumRecursion(someData){
-  if (someData === 0){
+function sumRecursion(someData) {
+  if (someData === 0) {
     return 0;
-  } else {
-    return (sumRecursion(someData - 1)) + 1);
   }
-  return 0;
+  return (sumRecursion(someData - 1) + 1);
 }
 
 function sumTheSimpleWay(someData) {
-  return (_.reduce(someData));
+  return _.reduce(someData);
 }
 
-// noinspection JSUnusedGlobalSymbols
+
 export default {
-  sumFor, sumWhile, sumRecursion, sumTheSimpleWay
-}
+  sumFor, sumWhile, sumRecursion, sumTheSimpleWay,
+};
